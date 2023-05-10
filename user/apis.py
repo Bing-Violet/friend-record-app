a3ae2dbbd3de964c5439f48074cc986800dad156
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-05-10 11:40:29 +1000

    add error handling in user creation.

--- user/apis.py
+++ user/apis.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth import authenticate
 from django.http import Http404
+from django.db import IntegrityError
+
 
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 
 
 from .serializers import UserCreateSerializer
@@ -30,8 +33,11 @@ class UserCreateApi(APIView):
             tokens = get_tokens_for_user(user)
             serializer = UserCreateSerializer(user)
             return JsonResponse({"user":serializer.data, "tokens":tokens}, status=200)
+        except IntegrityError as e:
+            # assumption is that email requested is already registered
+            return JsonResponse({'message':'this email is already in use.'}, status=400)
         except:
-            return Http404
+            raise Http404
 
 class UserLoginApi(APIView):
     def post(self, request, format=None):
